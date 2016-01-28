74abc014d187a8bdd2706bafc56c704d4f02dc2b
heijari <niklas.heijari@gmail.com>
2016-01-28 21:54:36 +0200

    Update settings.py

--- imgservice/settings.py
+++ imgservice/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-MEDIA_ROOT = 'C:/Users/Nipsu/imgservice/imgservice/media/'
+MEDIA_ROOT = '/media/'
 MEDIA_URL = '/media/'
 ROOT_URLCONF = 'imgservice.urls'
 
@@ -103,5 +103,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = 'C:/Users/Nipsu/imgservice/imgservice/static/'
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
